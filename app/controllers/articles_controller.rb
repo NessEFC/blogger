5d76f2fb1d051f79894d51c18a7692d0dcd379f7
NessEFC <NessEFC@gmail.com>
2017-03-25 22:34:56 -0600

    add delete and edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,24 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to action: 'index'
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
     redirect_to article_path(@article)
   end
 
